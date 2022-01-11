e1f1f7c337e319201c79fc6bd914c7aef480e384
Ezra <ezra.dev98@gmail.com>
2022-01-11 17:34:14 +0300

    Read/list using router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ListEmployeesComponent from './components/ListEmployeesComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Hurray</h1>
-      </header> 
-    </div>
+    // <div>
+    //   <HeaderComponent/>
+    //     <div className="container">
+    //       <ListEmployeesComponent/>
+    //     </div>
+    //   <FooterComponent/>
+    // </div>
+
+<div>
+<Router>
+      <HeaderComponent />
+        <div className="container">
+            <Routes> 
+                  <Route path = "/" element = {<ListEmployeesComponent/>}/>
+                  <Route path = "/employees" element = {<ListEmployeesComponent/>}></Route>
+                
+            </Routes>
+        </div>
+      <FooterComponent />
+</Router>
+</div>
   );
 }
 
